ffb4ae8c59053330f9c6d358fa9fc847cfae4a2d
qnvitor <vitornqueiroz.dir@gmail.com>
2024-10-31 18:48:25 -0300

    fix controllers

--- app/controllers/internamentos_controller.rb
+++ app/controllers/internamentos_controller.rb
@@ -1,70 +1,52 @@
-class InternamentosController < ApplicationController
-  before_action :set_internamento, only: %i[ show edit update destroy ]
+class ExamesController < ApplicationController
+  before_action :set_exame, only: %i[ show update destroy ]
+  skip_before_action :verify_authenticity_token
 
-  # GET /internamentos or /internamentos.json
+  # GET /exames or /exames.json
   def index
-    @internamentos = Internamento.all
+    @exames = Exame.all
+    render json: @exames
   end
 
-  # GET /internamentos/1 or /internamentos/1.json
+  # GET /exames/1 or /exames/1.json
   def show
+    render json: @exame
   end
 
-  # GET /internamentos/new
-  def new
-    @internamento = Internamento.new
-  end
-
-  # GET /internamentos/1/edit
-  def edit
-  end
-
-  # POST /internamentos or /internamentos.json
+  # POST /exames or /exames.json
   def create
-    @internamento = Internamento.new(internamento_params)
-
-    respond_to do |format|
-      if @internamento.save
-        format.html { redirect_to internamento_url(@internamento), notice: "Internamento was successfully created." }
-        format.json { render :show, status: :created, location: @internamento }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @internamento.errors, status: :unprocessable_entity }
-      end
+    @exame = Exame.new(exame_params)
+    if @exame.save
+      render json: @exame, status: :created, location: @exame
+    else
+      render json: @exame.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /internamentos/1 or /internamentos/1.json
+  # PATCH/PUT /exames/1 or /exames/1.json
   def update
-    respond_to do |format|
-      if @internamento.update(internamento_params)
-        format.html { redirect_to internamento_url(@internamento), notice: "Internamento was successfully updated." }
-        format.json { render :show, status: :ok, location: @internamento }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @internamento.errors, status: :unprocessable_entity }
-      end
+    if @exame.update(exame_params)
+      render json: @exame, status: :ok, location: @exame
+    else
+      render json: @exame.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /internamentos/1 or /internamentos/1.json
+  # DELETE /exames/1 or /exames/1.json
   def destroy
-    @internamento.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to internamentos_url, notice: "Internamento was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @exame.destroy!
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_internamento
-      @internamento = Internamento.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def internamento_params
-      params.require(:internamento).permit(:consulta_id, :data_inicio, :data_fim, :medicamento, :quantidade_medicamento, :obs_monitoramento)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_exame
+    @exame = Exame.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def exame_params
+    params.require(:exame).permit(:consulta_id, :nome_exame, :status_exame, :resultado)
+  end
 end
